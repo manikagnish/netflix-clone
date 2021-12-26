ade46ad63667d5999b9d9e1f3a33ad65d1d65d54
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-26 19:26:49 +0530

    completed navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Banner from "./components/banner/Banner";
+import Nav from "./components/navbar/Nav";
 import Row from "./components/row/Row";
 import requests from "./request";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="./images/netflix-logo.png"
+        alt="netflix logo"
+        className="nav__logo"
+      />
+      <img
+        src="./images/netflix-avatar.png"
+        alt="netflix avatar"
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
